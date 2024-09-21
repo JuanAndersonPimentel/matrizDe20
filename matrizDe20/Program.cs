6c97743b52d54389d78f3a2ac5a6de9940d724b0
UserGPC <UserGPC@DESKTOP-4VVRAQ5>
2024-09-20 21:09:25 -0400

    Agregar archivos de proyecto.

---
+++ matrizDe20/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matrizDe20
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[20];
+
+			try
+			{
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = i * 5;
+                }
+                Console.WriteLine("el elemento array: ");
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.WriteLine($"el elemento {i}: {array[i]}");
+                }
+
+
+            }
+            catch (Exception)
+			{
+                Console.WriteLine("error: presione cualquier tecla para salir. ");
+			
+			}
+        }
+    }
+}
